f2e4709157c9c640149003518af4a9a498c1de04
Anatoliy <gromovoyanatoliy@gmail.com>
2018-03-26 21:19:35 +0300

    I have done the first lesson=)

--- Lesson_1_hard/src/js/main.js
+++ Lesson_1_hard/src/js/main.js
@@ -5,10 +5,10 @@ var
 
 num = num.toString().split('');
 for (let i = 0; i < 5; i++){
-  result= result * +num[i];
-  console.log((i+1) + " element" + " = " + num[i]);
+  result *= +num[i];
+  console.log( (i + 1) + " element" + " = " + num[i]);
 }
 
 console.log("Result = " + result);
-console.log("Result in a cube = " + Math.pow(result,3));
+alert("Result in a cube = " + Math.pow(result,3) );
 
