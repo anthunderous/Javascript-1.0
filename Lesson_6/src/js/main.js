a9b2c8e25ce6375866576037b079fd6fe3d7a414
Anatoliy <gromovoyanatoliy@gmail.com>
2018-04-03 23:46:20 +0300

    ''

--- Lesson_6/src/js/main.js
+++ Lesson_6/src/js/main.js
@@ -3,7 +3,6 @@ let
   openBtn = document.getElementById('open-btn'),
   mainInfo = document.getElementsByClassName('main-info')[0],
   leftMenu = mainInfo.getElementsByTagName('div'),
-  //Мне кажется более рациональное решение, нежели вариант с перечеслением всех классов - они же разные все)
   goodsItem = document.getElementsByClassName('goods-item'),
   mainFunctions = document.getElementsByClassName('main-functions')[0],
   btnTag = mainFunctions.getElementsByTagName('button'),
@@ -22,81 +21,7 @@ let
     mlEmployers: {},
     open: true,
     shopItems: [],
-    discount: true,
-    shopGoods: function chooseGoods() {
-      for (let i = 0; i < 5; i++) {
-        let typeProducts = prompt("Какой тип товара будем продавать?");
-
-        if (((typeof (typeProducts)) === 'string') && ((typeof (typeProducts)) !== null) && (typeProducts !== '') && (typeProducts.length <= 50)) {
-          mainList.mlShopGoods[i] = typeProducts;
-        } else {
-          alert("Вы не заполнили поле или ввели больше 50 символов");
-          i--;
-        }
-      }
-    },
-    workTime: function workTime(time) {
-      if ((time >= 8) && (time <= 20)) {
-        mainList.open = true;
-        console.log("Добро пожаловать!=)");
-      } else
-        mainList.open = false;
-    },
-    start: function start() {
-      budget = +prompt("Ваш бюджет?");
-      while (isNaN(budget) || budget == "" || budget == null) {
-        alert("Вы не ввели данные");
-        budget = +prompt("Ваш бюджет?");
-      }
-      shopName = prompt("Название вашего магазина?").toUpperCase();
-    },
-    getDailyBudget: function getDailyBudget() {
-      alert("Это ваш ежедневный бюджет: " + budget / 30)
-    },
-    getDiscount: function getDiscount(discount) {
-      if (discount) {
-        price *= 0.8;
-        console.log("Цена после скидки: " + price)
-      }
-    },
-    takeEmployers: function takeEmployers() {
-      for (let i = 0; i < 4; i++) {
-        let employersName = prompt("Введите имя сотрудника");
-        if (((typeof (employersName)) === 'string') && ((typeof (employersName)) !== null) && (employersName !== '')) {
-          mainList.mlEmployers[i] = i + 1 + " - " + employersName;
-        } else {
-          alert("Вы не заполнили поле");
-          i--;
-        }
-      }
-    },
-    chooseShopItems: function chooseShopItems() {
-      let items = prompt("Перечислите через запятую ваши товары", "");
-      choose();
-
-      function choose() {
-        if (((typeof (items)) === 'string') && ((typeof (items)) != null) && (items !== '')) {
-          mainList.shopItems = items.split(",");
-          mainList.shopItems.push(prompt("Подождите, ещё ", ""));
-          mainList.shopItems.sort();
-        } else {
-          alert("Вы неправильно заполнили поле");
-          items = prompt("Перечислите через запятую ваши товары", "");
-          choose();
-        }
-      }
-    },
-    showShopItems: function showShopItems() {
-      document.write("У нас вы можете купить: <br>");
-      mainList.shopItems.forEach(function (item, i, arr) {
-        document.write("  - " + (i + 1) + " - " + item + "<br>");
-      });
-    },
-    getMainList: function getMainList() {
-      for (let key in mainList) {
-        console.log("Наш магазин включает в себя: " + key);
-      }
-    }
+    discount: true
   };
 console.log(mainList);
 
